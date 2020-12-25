032c0dede83b06fed51642a3979cc34ae46392d2
ganle <albert@gmail.com>
2020-12-25 17:03:04 +0800

    删除多余导包

--- changgou_parent/changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/controller/WebSeckillOrderController.java
+++ changgou_parent/changgou_web/changgou_web_seckill/src/main/java/com/changgou/seckill/controller/WebSeckillOrderController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.sql.Time;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
